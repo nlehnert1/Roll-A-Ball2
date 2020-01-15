29836d77435aa5ce235179b4f6789d419e6b41d6
Chloe Lehnert <nlehnert2@unl.edu>
2020-01-15 16:05:35 -0600

    Fixed I think

---
+++ Assets/Pacer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacer : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    private float zMax = 7.5f;
+    private float zMin = -7.5f;
+    private int direction = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zNew = transform.position.z + direction * speed * Time.deltaTime;
+    }
+}
