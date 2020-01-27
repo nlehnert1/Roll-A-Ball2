005e0813af3c8bd263aa18f0a0748e578aab1874
Chloe Lehnert <nlehnert2@unl.edu>
2020-01-27 17:15:41 -0600

    Updated stuff

--- Assets/Scripts/MoveTowardsPlayer.cs
+++ Assets/Scripts/MoveTowardsPlayer.cs
@@ -6,11 +6,13 @@ public class MoveTowardsPlayer : MonoBehaviour
 {
 
     public Transform playerTransform;
+    private float speed = .05f;
 
     // Update is called once per frame
     void Update()
     {
         // Move towards player slowly
+        this.transform.Translate(((playerTransform.position + new Vector3(0f,0.5f,0f)) - transform.position) * speed * Time.deltaTime);
     }
 
     private void ApplyDamage()
