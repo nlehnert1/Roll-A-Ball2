7e5f84e8d89b64e8e7351f8ab11828f375391c46
Chloe Lehnert <nlehnert2@unl.edu>
2020-01-29 16:20:03 -0600

    Audio stuff?

--- Assets/Scripts/MoveTowardsPlayer.cs
+++ Assets/Scripts/MoveTowardsPlayer.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class MoveTowardsPlayer : MonoBehaviour
 {
+    public AudioSource deathSound;
 
     public Transform playerTransform;
     private float speed = .05f;
+
+    private void Start()
+    {
+        deathSound = gameObject.GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         // Move towards player slowly
-        this.transform.Translate(((playerTransform.position + new Vector3(0f,0.5f,0f)) - transform.position) * speed * Time.deltaTime);
+        this.transform.Translate(((playerTransform.position + new Vector3(0f, 0.5f, 0f)) - transform.position) * speed * Time.deltaTime);
     }
 
     private void ApplyDamage()
     {
+        deathSound.Play();
         gameObject.SetActive(false);
     }
 }
+
